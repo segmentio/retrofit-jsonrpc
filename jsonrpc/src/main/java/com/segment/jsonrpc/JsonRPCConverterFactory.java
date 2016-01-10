2b341e77f3964a7c58afcacc7e090ad33ca045ee
Prateek Srivastava <f2prateek@gmail.com>
2016-01-09 18:33:55 -0800

    checkstyle fixes

--- jsonrpc/src/main/java/com/segment/jsonrpc/JsonRPCConverterFactory.java
+++ jsonrpc/src/main/java/com/segment/jsonrpc/JsonRPCConverterFactory.java
@@ -5,8 +5,8 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
-import retrofit2.*;
 import retrofit2.Converter;
+import retrofit2.Retrofit;
 
 public class JsonRPCConverterFactory extends Converter.Factory {
   public static JsonRPCConverterFactory create() {
--- jsonrpc/src/main/java/com/segment/jsonrpc/Method.java
+++ jsonrpc/src/main/java/com/segment/jsonrpc/Method.java
@@ -1,11 +1,8 @@
 package com.segment.jsonrpc;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import okhttp3.HttpUrl;
 
-import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
